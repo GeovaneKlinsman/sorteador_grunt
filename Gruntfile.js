42124f963febf32a867268ebdff8a95b3c97912a
GeovaneKlinsman <geovanegondim91@gmail.com>
2024-12-16 14:02:07 -0300

    problema com @@

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-module.exports = function(grunt) {
+module.exports  = function (grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         less: {
@@ -12,14 +12,18 @@ module.exports = function(grunt) {
                     compress: true,
                 },
                 files: {
-                    'dist/styles/main.min.css': 'src/styles/main.less'
+                    'dist/styles/main.min.css':'src/styles/main.less' 
                 }
             }
         },
         watch: {
-            less: {
-                files: ['src/styles/**/*.less'],
+            less:{
+                files: ['src/styles/**/*.less'],  //qualquer arquivo .less, dentro de qualquer pasta dentro de styles será observado (watch).
                 tasks: ['less:development']
+            },
+            html: {
+                files: ['src/index.html'],
+                tasks: ['replace:dev']
             }
         },
         replace: {
@@ -29,6 +33,10 @@ module.exports = function(grunt) {
                         {
                             match: 'ENDERECO_DO_CSS',
                             replacement: './styles/main.css'
+                        },
+                        {
+                            match: 'ENDERECO_DO_JS',
+                            replacement: '../src/scripts/main.js'
                         }
                     ]
                 },
@@ -36,18 +44,64 @@ module.exports = function(grunt) {
                     {
                         expand: true,
                         flatten: true,
-                        src:['src/index.html'],
+                        src: ['src/index.html'],
                         dest: 'dev/'
                     }
                 ]
+            },
+            dist: {
+                options: {
+                    patterns: [
+                        {
+                            match: 'ENDERECO_DO_CSS',
+                            replacement: './styles/main.min.css'
+                        },
+                        {
+                            match: 'ENDERECO_DO_JS',
+                            replacement: './scripts/main.min.js'
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['prebuild/index.html'],
+                        dest: 'dist/'
+                    }
+                ]
+            }
+        },
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {
+                    'prebuild/index.html' : 'src/index.html'
+                }
+            }
+        },
+        clean: ['prebuild'],
+        uglify: {
+            target: {
+                files: {
+                    'dist/scripts/main.min.js' : 'src/scripts/main.js'
+                }
             }
         }
     })
 
+
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-replace');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
 
     grunt.registerTask('default', ['watch']);
-    grunt.registerTask('build', ['less:production']);
+    grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'replace:dist', 'clean', 'uglify']);
+
 }
